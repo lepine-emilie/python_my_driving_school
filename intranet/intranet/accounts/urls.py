5b1450603583ec003d99abf717b022353e93bd98
Emilie <emilie.lepine@epitech.eu>
2020-12-06 20:13:59 +0100

    finished prices page // added static management for css/js/img

--- intranet/intranet/accounts/urls.py
+++ intranet/intranet/accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('/accounts', views.home),
-    path('/prices', views.home),
+    path('accounts', views.home),
+    path('prices', views.prices),
 ]
